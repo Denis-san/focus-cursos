1741a30d3a83e0b9ec7b20d6e202bfbfee1b998a
Denis-san <denis.nt.nc@gmail.com>
2022-12-08 18:29:21 -0300

    adicionando a lib javafx

--- src/focuscursos/Aplicacao.java
+++ src/focuscursos/Aplicacao.java
@@ -1,7 +1,20 @@
 package focuscursos;
 
-public class Aplicacao {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Aplicacao extends Application{
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		primaryStage.setTitle("Hello world Application");
+	    primaryStage.setWidth(300);
+	    primaryStage.setHeight(200);
+
+	    primaryStage.show();
+	}
+	
 	public static void main(String[] args) {
-		System.out.println("Hello word!");
+	    Application.launch(args);
 	}
 }
